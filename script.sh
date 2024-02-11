75660a94f50e457a12ffc71a6fecc8aef95ac758
larderio <159699605+larderio@users.noreply.github.com>
2024-02-11 14:15:22 +0000

    Add files via upload

--- script.sh
+++ script.sh
@@ -13,6 +13,7 @@ echo "TCPKeepAlive yes" >>$C
 echo "X11Forwarding yes" >>$C
 echo "X11DisplayOffset 10" >>$C
 echo "XAuthLocation /usr/bin/xauth" >>$C
+echo "X11UseLocalhost no" >>$C
 echo "PubkeyAuthentication yes" >>$C
 echo "IgnoreRhosts yes" >>$C
 echo "HostbasedAuthentication no" >>$C
